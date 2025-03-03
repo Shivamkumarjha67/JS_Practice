f25dac4616e54a33f00448b2d15633f7ffb8e621
Shivam Kumar Jha <shkrjh@gmail.com>
2025-03-03 11:43:26 +0530

    [Shivam Kumar Jha] Add . Adding two dice number solution added

---
+++ SequencePracticeProblems/AddTwoDiceNumber.js
@@ -0,0 +1,4 @@
+var diceNumber1 = Math.floor(Math.random()*6) + 1;
+var diceNumber2 = Math.floor(Math.random()*6) + 1;
+
+console.log("Printing the result addition of two dice number: " + (diceNumber1 + diceNumber2));
